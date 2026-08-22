cf8cff8acea3f609a994e23a207d67358336b047
Nate <njgriffith1@gmail.com>
2026-08-22 15:18:06 -0400

    apply same logic to all galleries

--- src/app/travels/new-mexico/new-mexico.component.ts
+++ src/app/travels/new-mexico/new-mexico.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ImageService } from '../../services/image.service';
+import { NgFor, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-new-mexico',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgOptimizedImage],
   templateUrl: './new-mexico.component.html',
   styleUrl: './new-mexico.component.css'
 })
 export class NewMexicoComponent {
+  images: Record<string, any> = {};
+  imageKeys: string[] = [];
 
+  constructor(private imageService: ImageService) { }
+
+  ngOnInit() {
+    this.imageService.images$.subscribe((images: Record<string, any>) => {
+      this.images = Object.fromEntries(
+        Object.entries(images).filter(([key]) => key.startsWith('New Mexico'))
+      );
+      this.imageKeys = Object.keys(this.images);
+    });
+  }
 }
--- src/app/travels/north-carolina/north-carolina.component.ts
+++ src/app/travels/north-carolina/north-carolina.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ImageService } from '../../services/image.service';
+import { NgFor, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-north-carolina',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgOptimizedImage],
   templateUrl: './north-carolina.component.html',
   styleUrl: './north-carolina.component.css'
 })
 export class NorthCarolinaComponent {
+  images: Record<string, any> = {};
+  imageKeys: string[] = [];
 
+  constructor(private imageService: ImageService) { }
+  ngOnInit() {
+    this.imageService.images$.subscribe((images: Record<string, any>) => {
+      this.images = Object.fromEntries(
+        Object.entries(images).filter(([key]) => key.startsWith('Wisteria'))
+      );
+      this.imageKeys = Object.keys(this.images);
+    });
+  }
 }
